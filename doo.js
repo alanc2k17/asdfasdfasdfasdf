30a7b00efa6ae794318ae4aecce9c71e66be1057
Alan Chen <alanc2k17@gmail.com>
2017-03-12 23:07:57 -0400

    doo

--- doo.js
+++ doo.js
@@ -1,8 +1,10 @@
-var drawCircle = function(e) {
+var drawCircle = function(e, x, y) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    x = e.offsetX;
+    y = e.offsetY;
     
-    c.setAttribute("cx", e.offsetX);
-    c.setAttribute("cy", e.offsetY);
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
     c.setAttribute("r", 10);
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
@@ -27,7 +29,7 @@ var animate = function() {
 
     var requestID;
 
-    var move = function() {
+    var move = function(e) {
 	console.log(requestID);
 	foo = document.getElementsByTagName("circle");
 
@@ -35,32 +37,41 @@ var animate = function() {
 	    var c = foo[i];
 	    var cx = parseInt(c.getAttribute("cx"));
 	    var cy = parseInt(c.getAttribute("cy"));
+	    var r = parseInt(c.getAttribute("r"));
 
-	    if (cx < 460)
-		c.setAttribute("cx", cx + 1);
-	    else
-		c.setAttribute("cx", cx - 1);
+	    if (r < 2)
+		pic.removeChild(c);
 
-	    if (cy < 460)
-		c.setAttribute("cy", cy + 1);
-	    else
-		c.setAttribute("cy", cy - 1);
+	    if (x < 10 || x > 490)
+		c.xv = parseInt(-c.xv);
+
+	    if (y < 10 || y > 490)
+		c.yv = parseInt(-c.yv);	    
+
+	    if (cy == 250) {
+		c.setAttribute("r", r / 2);
+		var child = drawCircle(e.offsetX, e.offsetY);
+		child.setAttribute("r", r / 2);
+		child.xv = parseInt(-c.xv);
+		child.yv = parseInt(-c.yv);
+		pic.appendChild(child);
+	    }
 	}
 	requestID = window.requestAnimationFrame(move);
     }
     move();
 };
 
-var clear = function() {
-    while (pic.lastChild) {
-	pic.removeChild(pic.lastChild);
-    }
-};
+    var clear = function() {
+	while (pic.lastChild) {
+	    pic.removeChild(pic.lastChild);
+	}
+    };
 
-var pic = document.getElementById("vimage");
-var clr = document.getElementById("clr");
-var mv = document.getElementById("mv");
+    var pic = document.getElementById("vimage");
+    var clr = document.getElementById("clr");
+    var mv = document.getElementById("mv");
 
-pic.addEventListener("click", drawCircle);
-clr.addEventListener("click", clear);
-mv.addEventListener("click", animate);
+    pic.addEventListener("click", drawCircle);
+    clr.addEventListener("click", clear);
+    mv.addEventListener("click", animate);
